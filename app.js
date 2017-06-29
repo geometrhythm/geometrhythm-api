50abcacac52ab30b953c8b7e71a4db3d68c42489
Walter Scarborough <wscarborough@pivotal.io>
2017-06-29 12:00:06 +0900

    Added cors and eslint
    Signed-off-by: Douglas Blumeyer <dblumeyer@pivotal.io>

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT}!`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
